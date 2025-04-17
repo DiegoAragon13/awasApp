2a799e802a4a681ea4b1fa69cf37c78f9a5bd2ab
Jemima Reyes <21041289@itdurango.edu.mx>
2025-04-17 14:11:07 -0600

    Modificacion ligera del tema y mejora del diseño del bottom nav bar

--- lib/utils/themes/theme.dart
+++ lib/utils/themes/theme.dart
@@ -7,9 +7,15 @@ class ThemeApp {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: GoogleFonts.poppins().fontFamily,
+    splashColor: Colors.transparent,
     brightness: Brightness.light,
     primaryColor: const Color(0xFF213555),
+    shadowColor: const Color.fromARGB(30, 0, 0, 0),
     scaffoldBackgroundColor: Colors.white,
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xFF213555),
+      secondary: Color.fromARGB(255, 255, 255, 255), // Color más claro que el scaffold
+    ),
     textTheme: TextTheme(
       bodyLarge: TextStyle(
         fontSize: 14,
@@ -26,11 +32,22 @@ class ThemeApp {
   );
 
   static ThemeData darktTheme = ThemeData(
+    splashColor: Colors.transparent,
     useMaterial3: true,
     fontFamily: GoogleFonts.poppins().fontFamily,
     brightness: Brightness.dark,
     primaryColor: const Color(0xFF213555),
-    scaffoldBackgroundColor: Colors.black,
+    scaffoldBackgroundColor: const Color.fromARGB(255, 6, 6, 6),
+    shadowColor: const Color.fromARGB(39, 7, 7, 13),
+    colorScheme: const ColorScheme.dark(
+      primary: Color(0xFF213555),
+      secondary: Color.fromARGB(
+        255,
+        9,
+        9,
+        9,
+      ), // Color más claro que el scaffold
+    ),
     textTheme: TextTheme(
       bodyLarge: TextStyle(
         fontSize: 14,
--- lib/widgets/bottom_bar_widget.dart
+++ lib/widgets/bottom_bar_widget.dart
@@ -18,28 +18,47 @@ class _BottomBarWidgetState extends State<BottomBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.amber,
-      unselectedItemColor: Colors.blueGrey,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      decoration: BoxDecoration(
+        // borderRadius: BorderRadius.circular(40), // Mueve el borderRadius aquí
+        // border: Border.all(
+        //   color: const Color.fromARGB(255, 11, 11, 11), // Color del borde
+        //   width: 0.5, // Ancho del borde
+        // ),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).shadowColor,
+            spreadRadius: 0,
+            blurRadius: 20,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(40),
+        child: BottomNavigationBar(
+          backgroundColor:
+              Theme.of(
+                context,
+              ).colorScheme.secondary, //Para que tome el color del tema
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.blueGrey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Settings',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
+      ),
     );
   }
 }
